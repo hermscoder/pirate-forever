92f90610d31b48bfd2c82c949666bd055ca20fe1
Emerson Ribeiro <emersonrjr03@gmail.com>
2022-11-11 21:38:28 +0000

    Refactoring the project folders and implementing image rendering

---
+++ src/main/java/com/hermscoder/main/GamePanel.java
@@ -0,0 +1,62 @@
+package com.hermscoder.main;
+
+import com.hermscoder.inputs.KeyboardInputs;
+import com.hermscoder.inputs.MouseInputs;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class GamePanel extends JPanel {
+    private float xDelta = 100, yDelta = 100;
+    private BufferedImage img, subImg;
+
+
+    public GamePanel() {
+        MouseInputs mouseInputs = new MouseInputs(this);
+        importImage();
+        setPanelSize();
+        addKeyListener(new KeyboardInputs(this));
+        addMouseListener(mouseInputs);
+        addMouseMotionListener(mouseInputs);
+    }
+
+    private void importImage() {
+        InputStream is = getClass().getResourceAsStream("/player_sprites.png");
+        try {
+            img = ImageIO.read(is);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void setPanelSize() {
+        Dimension size = new Dimension(1280, 800);
+        setMinimumSize(size);
+        setPreferredSize(size);
+        setMaximumSize(size);
+    }
+
+    public void changeXDelta(int value) {
+        this.xDelta += value;
+    }
+
+    public void changeYDelta(int value) {
+        this.yDelta += value;
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        subImg = img.getSubimage(1 * 64, 8 * 40, 64, 40);
+        g.drawImage(subImg, (int) xDelta, (int) yDelta, 128, 80, null);
+    }
+
+    public void setRectPost(int x, int y) {
+        this.xDelta = x;
+        this.yDelta = y;
+    }
+}
