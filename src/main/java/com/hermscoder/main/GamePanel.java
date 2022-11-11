2151d0851e4912966efdd5ba1571a0beddc883f6
Emerson Ribeiro <emersonrjr03@gmail.com>
2022-11-11 22:07:58 +0000

    Adding multiple animation support and player constants

--- src/main/java/com/hermscoder/main/GamePanel.java
+++ src/main/java/com/hermscoder/main/GamePanel.java
@@ -2,6 +2,8 @@ package com.hermscoder.main;
 
 import com.hermscoder.inputs.KeyboardInputs;
 import com.hermscoder.inputs.MouseInputs;
+import com.hermscoder.utils.Constants;
+import com.hermscoder.utils.Constants.PlayerConstants;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
@@ -10,18 +12,35 @@ import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
 
+import static com.hermscoder.utils.Constants.Directions.*;
+import static com.hermscoder.utils.Constants.PlayerConstants.*;
+
 public class GamePanel extends JPanel {
     private float xDelta = 100, yDelta = 100;
-    private BufferedImage img, subImg;
-
+    private BufferedImage img;
+    private BufferedImage[][] animations;
+    private int animationTick, animationIndex, animationSpeed = 15;
+    private int playerAction = IDLE;
+    private int playerDirection = -1;
+    private boolean moving = false;
 
     public GamePanel() {
         MouseInputs mouseInputs = new MouseInputs(this);
         importImage();
+        loadAnimations();
         setPanelSize();
         addKeyListener(new KeyboardInputs(this));
         addMouseListener(mouseInputs);
         addMouseMotionListener(mouseInputs);
+    }
+
+    private void loadAnimations() {
+        animations = new BufferedImage[9][6];
+        for (int j = 0; j < animations.length; j++) {
+            for (int i = 0; i < animations[j].length; i++) {
+                animations[j][i] = img.getSubimage(i*64, j*40, 64, 40);
+            }
+        }
     }
 
     private void importImage() {
@@ -30,6 +49,12 @@ public class GamePanel extends JPanel {
             img = ImageIO.read(is);
         } catch (IOException e) {
             e.printStackTrace();
+        } finally {
+            try {
+                is.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 
@@ -40,23 +65,60 @@ public class GamePanel extends JPanel {
         setMaximumSize(size);
     }
 
-    public void changeXDelta(int value) {
-        this.xDelta += value;
+    public void setDirection(int direction) {
+        this.playerDirection = direction;
+        moving = true;
     }
 
-    public void changeYDelta(int value) {
-        this.yDelta += value;
+    public void setMoving(boolean moving) {
+        this.moving = moving;
+    }
+
+    public void updateAnimationTick() {
+        animationTick++;
+        if(animationTick >= animationSpeed) {
+            animationTick = 0;
+            animationIndex++;
+            if(animationIndex >= getSpriteAmount(playerAction)) {
+                animationIndex = 0;
+            }
+        }
     }
 
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        subImg = img.getSubimage(1 * 64, 8 * 40, 64, 40);
-        g.drawImage(subImg, (int) xDelta, (int) yDelta, 128, 80, null);
+        updateAnimationTick();
+        setAnimation();
+        updatePosition();
+        g.drawImage(animations[playerAction][animationIndex], (int) xDelta, (int) yDelta, 256, 160, null);
     }
 
-    public void setRectPost(int x, int y) {
-        this.xDelta = x;
-        this.yDelta = y;
+    private void updatePosition() {
+        if(moving) {
+            switch (playerDirection) {
+                case LEFT:
+                    xDelta -= 5;
+                    break;
+                case UP:
+                    yDelta -= 5;
+                    break;
+                case RIGHT:
+                    xDelta += 5;
+                    break;
+                case DOWN:
+                    yDelta += 5;
+                    break;
+            }
+        }
     }
+
+    private void setAnimation() {
+        if(moving)
+            playerAction = RUNNING;
+        else
+            playerAction = IDLE;
+    }
+
+
 }
